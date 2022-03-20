4f43b224f22375ec6316e65b10690fcca674223c
Alexandr Tishkin <alexandrey980222@gmail.com>
2022-03-21 01:05:38 +0700

    косметические правки

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,11 +1,11 @@
 //   -= Окно изменений =-
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close');
-let overlay  = document.querySelector('.overlay');
+let overlay = document.querySelector('.overlay');
 //   -= инпуты =-
 let inputName = document.querySelector('#input-name');
 let inputSubname = document.querySelector('#input-subname');
-let popupSubmit = document.querySelector('.popup__form');
+let popupForm = document.querySelector('.popup__form');
 //   -= имя профиля =-
 let profileName = document.querySelector('.profile__name');
 let profileSubname = document.querySelector('.profile__subname');
@@ -13,20 +13,22 @@ let profileSubname = document.querySelector('.profile__subname');
 
 // -= функции открытия закрытия =-
 function openPopup () {
-  overlay .classList.add('popup_opened');
-  inputName.setAttribute.value = profileName.textContent;
-  inputSubname.setAttribute.value = profileSubname.textContent;
+  inputName.value = profileName.textContent;
+  inputSubname.value = profileSubname.textContent;
+  overlay.classList.add('popup_opened');
 }
 function closePopup () {
-  overlay .classList.remove('popup_opened');
+  overlay.classList.remove('popup_opened');
 }
-function formSubmitHandler () {
-  profileName.innerHTML = inputName.value;
-  profileSubname.innerHTML = inputSubname.value;
+function formSubmitHandler (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileSubname.textContent = inputSubname.value;
   closePopup();
 }
 
+
 // -=  =-
-popupSubmit.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
